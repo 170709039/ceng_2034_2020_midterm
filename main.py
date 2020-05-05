5fefd3dab8551f5139e6570192f9fc06a2cad14b
Mutafa Şükrü Kapusuz <mustafasukrukapusuz@posta.mu.edu.tr>
2020-05-05 04:52:13 +0300

    Assigment 3 was completed ( cpu_count - loadavg_5min < 1; then exit )

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from sys import platform
+import multiprocessing
 
 
 print('MIDTERM 2034 - Student: Mustafa Şükrü Kapusuz - 170709039');
@@ -11,4 +12,18 @@ print('Current process id:' + str(os.getpid()))
 # Assigment 2
 ## If the running OS is linux; print loadavg.
 if(platform=='Linux'):
-    print(os.getloadavg())
+    print("Linux Load Avarages:" + str(os.getloadavg()))
+
+
+# Assigment 3
+## Take and print “5 min loadavg” value and cpu core count. If the loadavg value is near
+## (or close) to the cpu core count (hint: nproc - 5min loadavg < 1) then exit script.
+load_avg1, load_avg5, load_avg15 = os.getloadavg()
+
+# take CPU count
+cpu_count = multiprocessing.cpu_count()
+
+# If the loadavg values is near to CPU core count then exit script.
+if( (cpu_count - load_avg5) < 1 ):
+    exit()
+
